c336491c76f16d7fb8d9ff2c04a3fd1b38bd54c8
nino <nino.courtois@epfedu.fr>
2023-09-26 15:08:27 +0200

    TP1 NINO COURTOIS

---
+++ TP1_manipNombresint_COURTOIS/TP1_manipNombresint_COURTOIS/src/main/java/com/mycompany/tp1_manipnombresint_courtois/TP1_manipNombresint_COURTOIS.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.tp1_manipnombresint_courtois;
+
+import java.util.Scanner;
+
+
+public class TP1_manipNombresint_COURTOIS {
+
+    public static void main(String[] args) 
+    {
+        //SAISIE ET MANIPULATION DES NOMBRES
+        //1
+        /*
+        int entier1;
+        int entier2;
+        Scanner sc;
+        sc = new Scanner(System.in);
+        System.out.println("Quels est la valeur du premier nombre entier?");
+        entier1 = sc.nextInt();
+        System.out.println("Quels est la valeur du deuxième nombre entier?");
+        entier2 = sc.nextInt();
+        //2
+        System.out.println(entier1 + entier2);
+        System.out.println(entier1-entier2);
+        System.out.println(entier1*entier2);
+        //3
+        System.out.println(entier1/entier2);
+        System.out.println(entier1%entier2);
+         */
+        // SYSTEME DE TEMPERATURE
+        
+     //1
+      Scanner sc;  
+     sc = new Scanner(System.in);
+     System.out.println("valeur1 ?");
+     double valeur1 = sc.nextDouble();
+     System.out.println(valeur1);
+     //2
+     double valeur2;
+     valeur2 = valeur1 + 273;
+     System.out.println(valeur2);
+     //3
+     
+     
+      
+      
+      
+    }
+}
