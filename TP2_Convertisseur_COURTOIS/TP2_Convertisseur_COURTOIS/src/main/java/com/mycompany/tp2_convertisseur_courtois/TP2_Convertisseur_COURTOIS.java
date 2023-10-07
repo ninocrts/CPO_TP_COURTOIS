0f045501f87b8bf85fcbb70f17457be90216d587
nino <nino.courtois@epfedu.fr>
2023-10-07 19:35:51 +0200

    TP2 NINO COURTOIS temperature

---
+++ TP2_Convertisseur_COURTOIS/TP2_Convertisseur_COURTOIS/src/main/java/com/mycompany/tp2_convertisseur_courtois/TP2_Convertisseur_COURTOIS.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.tp2_convertisseur_courtois;
+
+
+public class TP2_Convertisseur_COURTOIS {
+
+    public static void main(String[] args) {
+        
+    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+       
+    }
+}
