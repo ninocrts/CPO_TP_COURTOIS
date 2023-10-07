60540fd3670fb08b4e317d6333a186a30eda365e
nino <nino.courtois@epfedu.fr>
2023-10-07 18:20:58 +0200

    TP2 NINO COURTOIS

---
+++ TP2_Bieres_COURTOIS/TP2_Bieres_COURTOIS/src/main/java/com/mycompany/tp2_bieres_courtois/TP2_Bieres_COURTOIS.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.tp2_bieres_courtois;
+
+
+
+public class TP2_Bieres_COURTOIS {
+
+    public static void main(String[] args) {
+      
+  BouteilleBiere uneBiere = new BouteilleBiere() ;
+
+}
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
