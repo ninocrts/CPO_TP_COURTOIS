74492521bed5b4f81cbda199562f1bdd3cd8fe7b
nino <nino.courtois@epfedu.fr>
2023-10-11 10:41:28 +0200

    TP2 NINO COURTOIS manip :3

---
+++ New Folder (2)/TP3_Manipulation_des_objets/src/tp3_manipulation_des_objets/TP3_Manipulation_des_objets.java
@@ -0,0 +1,20 @@
+ /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp3_manipulation_des_objets;
+
+/**
+ *
+ * @author utilisateur
+ */
+public class TP3_Manipulation_des_objets {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
