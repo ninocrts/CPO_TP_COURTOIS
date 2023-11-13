51c0ca2317212cefb70aaadaaf8bde2e8285a3c4
nino <nino.courtois@epfedu.fr>
2023-11-13 14:43:08 +0100

    TP Lights Off Nino COURTOIS version graphique

--- Projet_Lights_Off_Version_Graphique/LightOff_COURTOIS_Nino_version_graphique/src/FenetrePrincipale.java
+++ Projet_Lights_Off_Version_Graphique/LightOff_COURTOIS_Nino_version_graphique/src/FenetrePrincipale.java
@@ -1,69 +1,84 @@
 
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JButton;
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-
 public class FenetrePrincipale extends javax.swing.JFrame {
-    
+
     /**
      * Creates new form FenetrePrincipale
      */
-     int nbCoups;
-     GrilleDeJeu grille;
-     
+    int nbCoups;
+    int i;
+    GrilleDeJeu grille;
+
     public void initialiserPartie() {
-    grille.eteindreToutesLesCellules();
-    grille.melangerMatriceAleatoirement(10);
+        grille.eteindreToutesLesCellules();
+        grille.melangerMatriceAleatoirement(10);
     }
-        
+
     public FenetrePrincipale() {
-        
+
         int nbLignes = 10;
         int nbColonnes = 10;
-        
-        PanneauBoutonsVerticaux.setLayout(new GridLayout(nbLignes, 1));
-        getContentPane().add(PanneauGrille, new
-        org.netbeans.lib.awtextra.AbsoluteConstraints(20, 20, 1 * 40, nbLignes * 40));
-        this.pack();
-        this.revalidate();
 
-        initComponents();
-        
-        this.grille = new GrilleDeJeu(nbLignes, nbColonnes);
-        PanneauGrille.setLayout(new GridLayout(nbLignes, nbColonnes));
-        for (int i=0; i < nbLignes; i++) {
-        for (int j=0; j < nbColonnes; j++ ) {
-        //JButton bouton_cellule = new JButton(); // création d'un bouton
-        CelluleGraphique bouton_cellule = new CelluleGraphique( grille.matriceCellules[i][j], 36,36);
-        PanneauGrille.add(bouton_cellule); // ajout au Jpanel PanneauGrille
         PanneauBoutonsVerticaux.setLayout(new GridLayout(nbLignes, 1));
         getContentPane().add(PanneauGrille, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 20, 1 * 40, nbLignes * 40));
         this.pack();
         this.revalidate();
 
- }
-}
+        initComponents();
+
+        this.grille = new GrilleDeJeu(nbLignes, nbColonnes);
+        PanneauGrille.setLayout(new GridLayout(nbLignes, nbColonnes));
+        for (int i = 0; i < nbLignes; i++) {
+            for (int j = 0; j < nbColonnes; j++) {
+                //JButton bouton_cellule = new JButton(); // création d'un bouton
+                CelluleGraphique bouton_cellule = new CelluleGraphique(grille.matriceCellules[i][j], 36, 36);
+                PanneauGrille.add(bouton_cellule); // ajout au Jpanel PanneauGrille
+            
+            }
+        }
+                
+                PanneauBoutonsVerticaux.setLayout(new GridLayout(nbLignes, 1));
+                getContentPane().add(PanneauGrille, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 20, 1 * 40, nbLignes * 40));
+                this.pack();
+                this.revalidate();
+
+                // création du panneau de boutons verticaux (pour les lignes)
+                for (i = 0; i < nbLignes; i++) {
+                    JButton bouton_ligne = new JButton();
+                    ActionListener ecouteurClick = new ActionListener() {
+                        final int j = i;
+
+                        @Override
+                        public void actionPerformed(ActionEvent e) {
+                            grille.activerLigneDeCellules(j);
+                            repaint();
+                        }
+                    };
+
+                    bouton_ligne.addActionListener(ecouteurClick);
+                    PanneauBoutonsVerticaux.add(bouton_ligne);
+
+                }
+
+            
+        
 
     }
+
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
         PanneauGrille = new javax.swing.JPanel();
         jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
-        jButton4 = new javax.swing.JButton();
-        jButton5 = new javax.swing.JButton();
-        jButton6 = new javax.swing.JButton();
-        jButton7 = new javax.swing.JButton();
-        jButton8 = new javax.swing.JButton();
-        jButton9 = new javax.swing.JButton();
-        jButton10 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
@@ -93,133 +108,15 @@ public class FenetrePrincipale extends javax.swing.JFrame {
         });
         getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 60, 50, 30));
 
-        jButton2.setText("btnLigne0");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
-            }
-        });
-        getContentPane().add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 90, 50, 30));
-
-        jButton3.setText("btnLigne0");
-        jButton3.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton3ActionPerformed(evt);
-            }
-        });
-        getContentPane().add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 130, 50, 30));
-
-        jButton4.setText("btnLigne0");
-        jButton4.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton4ActionPerformed(evt);
-            }
-        });
-        getContentPane().add(jButton4, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 160, 50, 30));
-
-        jButton5.setText("btnLigne0");
-        jButton5.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton5ActionPerformed(evt);
-            }
-        });
-        getContentPane().add(jButton5, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 200, 50, 30));
-
-        jButton6.setText("btnLigne0");
-        jButton6.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton6ActionPerformed(evt);
-            }
-        });
-        getContentPane().add(jButton6, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 230, 50, 30));
-
-        jButton7.setText("btnLigne0");
-        jButton7.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton7ActionPerformed(evt);
-            }
-        });
-        getContentPane().add(jButton7, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 270, 50, 30));
-
-        jButton8.setText("btnLigne0");
-        jButton8.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton8ActionPerformed(evt);
-            }
-        });
-        getContentPane().add(jButton8, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 300, 50, 30));
-
-        jButton9.setText("btnLigne0");
-        jButton9.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton9ActionPerformed(evt);
-            }
-        });
-        getContentPane().add(jButton9, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 340, 50, 30));
-
-        jButton10.setText("btnLigne0");
-        jButton10.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton10ActionPerformed(evt);
-            }
-        });
-        getContentPane().add(jButton10, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 370, 50, 30));
-
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-    this.grille.activerLigneDeCellules(0);
-    repaint();
+        this.grille.activerLigneDeCellules(0);
+        repaint();
         // TODO add your handling code here:
     }//GEN-LAST:event_jButton1ActionPerformed
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-    this.grille.activerLigneDeCellules(1);
-    repaint();   // TODO add your handling code here:
-    }//GEN-LAST:event_jButton2ActionPerformed
-
-    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-    this.grille.activerLigneDeCellules(2);
-    repaint();   // TODO add your handling code here:
-    }//GEN-LAST:event_jButton3ActionPerformed
-
-    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
-    this.grille.activerLigneDeCellules(3);
-    repaint();   // TODO add your handling code here:
-    }//GEN-LAST:event_jButton4ActionPerformed
-
-    private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
-    this.grille.activerLigneDeCellules(4);
-    repaint();    // TODO add your handling code here:
-    }//GEN-LAST:event_jButton5ActionPerformed
-
-    private void jButton6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton6ActionPerformed
-    this.grille.activerLigneDeCellules(5);
-    repaint();    // TODO add your handling code here:
-    }//GEN-LAST:event_jButton6ActionPerformed
-
-    private void jButton7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton7ActionPerformed
-    this.grille.activerLigneDeCellules(6);
-    repaint();    // TODO add your handling code here:
-    }//GEN-LAST:event_jButton7ActionPerformed
-
-    private void jButton8ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton8ActionPerformed
-    this.grille.activerLigneDeCellules(7);
-    repaint();    // TODO add your handling code here:
-    }//GEN-LAST:event_jButton8ActionPerformed
-
-    private void jButton9ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton9ActionPerformed
-    this.grille.activerLigneDeCellules(8);
-    repaint();    // TODO add your handling code here:
-    }//GEN-LAST:event_jButton9ActionPerformed
-
-    private void jButton10ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton10ActionPerformed
-    this.grille.activerLigneDeCellules(9);
-    repaint();    // TODO add your handling code here:
-    }//GEN-LAST:event_jButton10ActionPerformed
-
-    
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
@@ -253,15 +150,6 @@ public class FenetrePrincipale extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel PanneauGrille;
     private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton10;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
-    private javax.swing.JButton jButton4;
-    private javax.swing.JButton jButton5;
-    private javax.swing.JButton jButton6;
-    private javax.swing.JButton jButton7;
-    private javax.swing.JButton jButton8;
-    private javax.swing.JButton jButton9;
     // End of variables declaration//GEN-END:variables
 
     private static class PanneauBoutonsVerticaux {
@@ -270,8 +158,12 @@ public class FenetrePrincipale extends javax.swing.JFrame {
             throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
         }
 
+        private static void add(JButton bouton_ligne) {
+            throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        }
+
         public PanneauBoutonsVerticaux() {
-            
+
         }
     }
 }
